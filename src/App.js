a6cd2c7f65f996fae0269f0285496fff7e9a36a0
salihu <57968062+salihuabubakar@users.noreply.github.com>
2022-02-26 17:04:21 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [inputText, setInputText] = useState("");
+   const [todos, setTodos] = useState([]);
+
+  const handleChange = (event) => {
+    const inputValue = event.target.value;
+    setInputText(inputValue)
+  }
+
+   const saveToLocalStorage = (todo) => {
+     localStorage.setItem("Todos", JSON.stringify(todo));
+   };
+
+    useEffect(() => {
+      let getTodoFromLocalStorage = localStorage.getItem("Todos");
+      getTodoFromLocalStorage = JSON.parse(getTodoFromLocalStorage);
+      if (getTodoFromLocalStorage) {
+        setTodos(getTodoFromLocalStorage);
+      }
+    }, []);
+
+  const handleBtn = () => {
+    const newTodo = (todo)=>{
+      return [...todo, inputText];
+    } 
+    setTodos(newTodo)
+    saveToLocalStorage(newTodo(todos));
+    setInputText("")
+  }
+
+  const removeTodoBtn = (id) => {
+    const todoRemove = todos.filter((item, index) => {
+      return index !== id;
+    }) 
+    setTodos(todoRemove)
+    console.log(id)
+    //saveToLocalStorage(setTodos(todos))
+  }
+
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Heading">
+        <h1>Todo's</h1>
+      </div>
+      <div className="InputBox">
+        <input name="input" onChange={handleChange} value={inputText} type="text" />
+        <button onClick={handleBtn}>Add</button>
+      </div>
+      <div className="Contents">
+        <ul>
+          {todos.map((item, index) => {
+           return <li id={index} key={index}>{item} <span> <Button onClick={() => removeTodoBtn(index)}>Delete</Button> </span></li>
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
